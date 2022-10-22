0b7cd3224e6a6d5049d412120045467fbeaf7c5a
Tobiasz-T <tobi1997tobi@wp.pl>
2022-10-22 23:42:16 +0200

    Zmiana

--- code.js
+++ code.js
@@ -42,9 +42,9 @@ var skills=
                     <div id="right">\
                         <h2>My Web Pages</h2>\
                         <ul>\
-                            <li><a href=""><h3>Portfolio(this page)</h3></a></li>\
-                            <li><a href=""><h3>ROCK_PAPER_SCISSORS</h3></a></li>\
-                            <li><a href=""><h3>TIC_TAK_TOE</h3></a></li>\
+                            <li><a href="https://github.com/Tobiasz-T/Portfolio" target="blank"><h3>Portfolio(this page)</h3></a></li>\
+                            <li><a href="https://github.com/Tobiasz-T/rock_paper_scissors" target="blank"><h3>ROCK_PAPER_SCISSORS</h3></a></li>\
+                            <li><a href="https://github.com/Tobiasz-T/tic_tac_toe_2" target="blank"><h3>TIC_TAK_TOE</h3></a></li>\
                         </ul>\
                         <h2>Other Skills</h2>\
                         <ul>\
